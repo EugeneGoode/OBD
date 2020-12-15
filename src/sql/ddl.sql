3ae9ddd77536a882179875856d75f575da2a29c5
EugeneGoode <66566549+EugeneGoode@users.noreply.github.com>
2020-12-15 17:44:12 +0200

    Update ddl.sql

--- src/sql/ddl.sql
+++ src/sql/ddl.sql
@@ -3,270 +3,146 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema NakedSun
+-- Schema mydb
 -- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema questionnaire
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `questionnaire` ;
+DROP SCHEMA IF EXISTS `mydb` ;
 
 -- -----------------------------------------------------
--- Schema questionnaire
+-- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `questionnaire` ;
-USE `questionnaire` ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `questionnaire`.`survey`
+-- Table `mydb`.`ServiceNode`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `questionnaire`.`survey` ;
-
-CREATE TABLE IF NOT EXISTS `questionnaire`.`survey` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) CHARACTER SET 'utf8' NOT NULL,
-  `topic` VARCHAR(45) CHARACTER SET 'utf8' NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `mydb`.`ServiceNode` (
+  `url` TEXT NOT NULL,
+  `name` TEXT NULL,
+  `description` TEXT NULL,
+  `OpenApi` TEXT NULL,
+  PRIMARY KEY (`url`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `questionnaire`.`question`
+-- Table `mydb`.`ServiceChain`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `questionnaire`.`question` ;
-
-CREATE TABLE IF NOT EXISTS `questionnaire`.`question` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `text` VARCHAR(150) CHARACTER SET 'utf8' NOT NULL,
-  `survey_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`ServiceChain` (
+  `id` INT NOT NULL,
+  `next` TEXT NULL,
+  `ServiceNode_url` TEXT NOT NULL,
+  `ServiceChain_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_Question_survey1_idx` (`survey_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Question_survey1`
-    FOREIGN KEY (`survey_id`)
-    REFERENCES `questionnaire`.`survey` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  INDEX `fk_ServiceChain_ServiceNode1_idx` (`ServiceNode_url` ASC) VISIBLE,
+  INDEX `fk_ServiceChain_ServiceChain1_idx` (`ServiceChain_id` ASC) VISIBLE,
+  CONSTRAINT `fk_ServiceChain_ServiceNode1`
+    FOREIGN KEY (`ServiceNode_url`)
+    REFERENCES `mydb`.`ServiceNode` (`url`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ServiceChain_ServiceChain1`
+    FOREIGN KEY (`ServiceChain_id`)
+    REFERENCES `mydb`.`ServiceChain` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `questionnaire`.`customer`
+-- Table `mydb`.`Stream`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `questionnaire`.`customer` ;
+CREATE TABLE IF NOT EXISTS `mydb`.`Stream` (
+  `name` INT NOT NULL,
+  `description` TEXT NULL,
+  `etc` TEXT NULL,
+  `ServiceChain_id` INT NOT NULL,
+  PRIMARY KEY (`name`),
+  INDEX `fk_Stream_ServiceChain1_idx` (`ServiceChain_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Stream_ServiceChain1`
+    FOREIGN KEY (`ServiceChain_id`)
+    REFERENCES `mydb`.`ServiceChain` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `questionnaire`.`customer` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) CHARACTER SET 'utf8' NOT NULL,
-  `email` VARCHAR(320) CHARACTER SET 'utf8' NOT NULL,
-  `password` VARCHAR(45) CHARACTER SET 'utf8' NOT NULL,
-  `role` VARCHAR(45) NOT NULL,
+
+-- -----------------------------------------------------
+-- Table `mydb`.`BeatType`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`BeatType` (
+  `name` TEXT NOT NULL,
+  `installer` TEXT NULL,
+  PRIMARY KEY (`name`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`beat`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`beat` (
+  `id GUID` INT NOT NULL,
+  `interval` INT NULL,
+  `Stream_name` INT NOT NULL,
+  `BeatType_name` TEXT NOT NULL,
+  PRIMARY KEY (`id GUID`),
+  INDEX `fk_beat_Stream1_idx` (`Stream_name` ASC) VISIBLE,
+  INDEX `fk_beat_BeatType1_idx` (`BeatType_name` ASC) VISIBLE,
+  CONSTRAINT `fk_beat_Stream1`
+    FOREIGN KEY (`Stream_name`)
+    REFERENCES `mydb`.`Stream` (`name`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_beat_BeatType1`
+    FOREIGN KEY (`BeatType_name`)
+    REFERENCES `mydb`.`BeatType` (`name`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Source`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Source` (
+  `url` TEXT NOT NULL,
+  `password` TEXT NULL,
+  `username` TEXT NULL,
+  `apikey` TEXT NULL,
+  `beat_id GUID` INT NOT NULL,
+  PRIMARY KEY (`url`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
+  INDEX `fk_Source_beat_idx` (`beat_id GUID` ASC) VISIBLE,
+  CONSTRAINT `fk_Source_beat`
+    FOREIGN KEY (`beat_id GUID`)
+    REFERENCES `mydb`.`beat` (`id GUID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`User`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`User` (
+  `id` INT NOT NULL,
+  `password` TEXT NULL,
+  `email` TEXT NULL,
+  `status` INT NULL,
+  `username` VARCHAR(45) NULL,
+  `beat_id GUID` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `questionnaire`.`expert`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `questionnaire`.`expert` ;
-
-CREATE TABLE IF NOT EXISTS `questionnaire`.`expert` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `expert_user1_idx` (`expert_id` ASC) VISIBLE,
-  CONSTRAINT `expert_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `questionnaire`.`customer` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `questionnaire`.`correctAnswers`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `questionnaire`.`correctAnswers` ;
-
-CREATE TABLE IF NOT EXISTS `questionnaire`.`correctAnswers` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `text` VARCHAR(150) CHARACTER SET 'utf8' NOT NULL,
-  `expert_id` INT NOT NULL,
-  `question_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `correctAnswers_expert1_idx` (`expert_id` ASC) VISIBLE,
-  INDEX `correctAnswers_Question1_idx` (`Question_id` ASC) VISIBLE,
-  CONSTRAINT `correctAnswers_Question1`
-    FOREIGN KEY (`Question_id`)
-    REFERENCES `questionnaire`.`question` (`id`),
-  CONSTRAINT `correctAnswers_expert1`
-    FOREIGN KEY (`respondent_id`)
-    REFERENCES `questionnaire`.`expert` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `questionnaire`.`incorrectAnswers`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `questionnaire`.`incorrectAnswers` ;
-
-CREATE TABLE IF NOT EXISTS `questionnaire`.`incorrectAnswers` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `text` VARCHAR(150) CHARACTER SET 'utf8' NOT NULL,
-  `question_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `Question_id`),
-  INDEX `incorrectAnswers_Question1_idx` (`Question_id` ASC) VISIBLE,
-  CONSTRAINT `incorrectAnswers_Question1`
-    FOREIGN KEY (`Question_id`)
-    REFERENCES `questionnaire`.`question` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-DROP TABLE IF EXISTS `questionnaire`.`role` ;
-
-CREATE TABLE IF NOT EXISTS `questionnaire`.`role` (
-                                                 `id` INT NOT NULL,
-                                                 `name` TEXT NOT NULL,
-                                                 PRIMARY KEY (`id`),
-                                                 UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
-    ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `questionnaire`.`report`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `questionnaire`.`report` ;
-
-CREATE TABLE IF NOT EXISTS `questionnaire`.`report` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `text` VARCHAR(150) CHARACTER SET 'utf8' NOT NULL,
-  `survey_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `survey_id`, `user_id`),
-  INDEX `report_survey1_idx` (`survey_id` ASC) VISIBLE,
-  INDEX `report_user1_idx` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `report_survey1`
-    FOREIGN KEY (`survey_id`)
-    REFERENCES `questionnaire`.`survey` (`id`),
-  CONSTRAINT `report_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `questionnaire`.`expert` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `status_UNIQUE` (`status` ASC) VISIBLE,
+  INDEX `fk_User_beat1_idx` (`beat_id GUID` ASC) VISIBLE,
+  CONSTRAINT `fk_User_beat1`
+    FOREIGN KEY (`beat_id GUID`)
+    REFERENCES `mydb`.`beat` (`id GUID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
--- -----------------------------------------------------
--- Data for table `questionnaire`.`survey`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `questionnaire`;
-INSERT INTO `questionnaire`.`survey` (`id`, `name`, `Topic`) VALUES (1, 'Sigmund Freud', 'Psychology');
-INSERT INTO `questionnaire`.`survey` (`id`, `name`, `Topic`) VALUES (2, 'What animal are you', 'Nature');
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `questionnaire`.`question`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `questionnaire`;
-INSERT INTO `questionnaire`.`question` (`id`, `text`, `survey_id`) VALUES (1, 'When he was born?', 1);
-INSERT INTO `questionnaire`.`question` (`id`, `text`, `survey_id`) VALUES (2, 'Which country is his home country?', 1);
-INSERT INTO `questionnaire`.`question` (`id`, `text`, `survey_id`) VALUES (3, 'Death date', 2);
-INSERT INTO `questionnaire`.`question` (`id`, `text`, `survey_id`) VALUES (4, 'Did he have a wife?', 2);
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `questionnaire`.`expert`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `questionnaire`;
-INSERT INTO `questionnaire`.`expert` (`id`) VALUES (1);
-INSERT INTO `questionnaire`.`expert` (`id`) VALUES (2);
-INSERT INTO `questionnaire`.`expert` (`id`) VALUES (3);
-INSERT INTO `questionnaire`.`expert` (`id`) VALUES (4);
-INSERT INTO `questionnaire`.`expert` (`id`) VALUES (5);
-INSERT INTO `questionnaire`.`expert` (`id`) VALUES (6);
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `questionnaire`.`customer`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `questionnaire`;
-INSERT INTO `questionnaire`.`customer` (`id`, `specialization_survay`, `name`, `email`, `password`,) VALUES (0.1, 'Psychology', 'Гладков Даніїл', 'gladkovdaniil.kpi.io92@gmail.com', '123346');
-INSERT INTO `questionnaire`.`customer` (`id`, `specialization_survay`, `name`, `email`, `password`,) VALUES (0.2, 'Nature', 'Карнаухова Анастасія', 'anastasiz.bbq@gmail.com', 'Loremipsum');
-INSERT INTO `questionnaire`.`customer` (`id`, `specialization_survay`, `name`, `email`, `password`,) VALUES (0.3, 'Psychology', 'Кушенко Сергій', 'kushenkosergey111@gmail.com', 'Bababoy56');
-INSERT INTO `questionnaire`.`customer` (`id`, `specialization_survay`, `name`, `email`, `password`,) VALUES (0.4, 'Psychology', 'Педенко Данило', 'pedenko.danil@gmail.com', 'Sobaka_Ubivaka');
-INSERT INTO `questionnaire`.`customer` (`id`, `specialization_survay`, `name`, `email`, `password`,) VALUES (0.5, 'Nature', 'Рожко Михайло', 'mihaylo.rozhko.01@gmail.com', 'Starosta_24_com');
-INSERT INTO `questionnaire`.`customer` (`id`, `specialization_survay`, `name`, `email`, `password`,) VALUES (0.6, 'Nature', 'Соболь Денис', 'deesy2203@gmail.com', 'winner_2001_best');
-
-COMMIT;
-
-START TRANSACTION;
-USE `NakedSun`;
-INSERT INTO `questionnaire`.`role` (`id`, `name`) VALUES (1, 'Manager');
-INSERT INTO `questionnaire`.`role` (`id`, `name`) VALUES (2, 'Analyst');
-INSERT INTO `questionnaire`.`role` (`id`, `name`) VALUES (3, 'Expert');
-INSERT INTO `questionnaire`.`role` (`id`, `name`) VALUES (3, 'Customer');
-
-COMMIT;
--- -----------------------------------------------------
--- Data for table `questionnaire`.`correctAnswers`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `questionnaire`;
-INSERT INTO `questionnaire`.`correctAnswers` (`id`, `text`, `expert_id`, `Question_id`) VALUES (1, 'Blue', 1, 1);
-INSERT INTO `questionnaire`.`correctAnswers` (`id`, `text`, `expert_id`, `Question_id`) VALUES (2, '1856', 1, 2);
-INSERT INTO `questionnaire`.`correctAnswers` (`id`, `text`, `expert_id`, `Question_id`) VALUES (3, 'I don`t know', 2, 3);
-INSERT INTO `questionnaire`.`correctAnswers` (`id`, `text`, `expert_id`, `Question_id`) VALUES (4, 'Im just Lorem Ipsum', 2, 4);
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `questionnaire`.`incorrectAnswers`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `questionnaire`;
-INSERT INTO `questionnaire`.`incorrectAnswers` (`id`, `text`, `Question_id`) VALUES (1, 'Red', 1);
-INSERT INTO `questionnaire`.`incorrectAnswers` (`id`, `text`, `Question_id`) VALUES (2, '1855', 2);
-INSERT INTO `questionnaire`.`incorrectAnswers` (`id`, `text`, `Question_id`) VALUES (3, 'I know', 3);
-INSERT INTO `questionnaire`.`incorrectAnswers` (`id`, `text`, `Question_id`) VALUES (4, 'Boy next door', 4);
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `questionnaire`.`report`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `questionnaire`;
-INSERT INTO `questionnaire`.`report` (`id`, `text`, `survey_id`, `user_id`) VALUES (1, '1 star(', 1, 2);
-INSERT INTO `questionnaire`.`report` (`id`, `text`, `survey_id`, `user_id`) VALUES (2, '5 stars!', 2, 2);
-
-COMMIT;
